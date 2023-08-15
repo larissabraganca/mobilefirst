6c6649ebe508f31af21889515dcc23ff0c7cd302
Larissa Bragança <126626998+larissabraganca@users.noreply.github.com>
2023-08-14 21:38:10 -0300

    Add files via upload

---
+++ mobilefirst/mobilefirst/main.js
@@ -0,0 +1,12 @@
+/*var hamburguer = document.querySelector("#menu");
+
+var menu = document.querySelector(".lista_menu");
+
+hamburguer.addEventListener('click', function(){
+ menu.classList.add('menu_open');
+})*/
+
+/*document.querySelector(".titulo_banner").innerHTML = "Hello World!";*/
+
+
+
